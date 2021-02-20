f7a498fbd85f8676c824820ff498e22092774225
mulholio <james@jmulholland.com>
2021-02-20 19:45:54 +0000

    do basic weather fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { API_KEY } from './key';
 
-console.log('working', API_KEY)
+const CITY = 'London,uk'
+
+const ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`
+
+fetch(ENDPOINT)
+  .then(res => res.json())
+  .then(x => console.log(x))
--- src/key.example.js
+++ src/key.example.js
@@ -1,1 +1,1 @@
-const API_KEY = '124124asdf70asdf70a0das0dfadfs'
+export const API_KEY = '124124asdf70asdf70a0das0dfadfs'
