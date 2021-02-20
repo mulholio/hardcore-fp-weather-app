691dd0cd37b0a1ca96fa9da343edbcef947e25ea
mulholio <james@jmulholland.com>
2021-02-20 21:00:49 +0000

    make fetch functional; config webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import { API_KEY } from './key';
+import { API_KEY } from './key'
+import { Task } from './types'
+import { compose } from 'ramda'
 
-const CITY = 'London,uk'
+const mkUrl = city => 
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
-const ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`
+/** functional fetch */
+const fpetch = url =>
+  Task((rej, res) => fetch(url)
+    .then(res)
+    .catch(rej)
+  )
 
-fetch(ENDPOINT)
-  .then(res => res.json())
-  .then(x => console.log(x))
+const getJson = response =>
+  Task((_rej, res) => response.json().then(data => res(data)))
+
+const getData = url =>
+  fpetch(url)
+  .chain(getJson)
+
+// TODO render cloud coverage
+// TODO render wind speed and direction
+// TODO render sunrise/sunset
+// TODO render temp in K
+// TODO render temp in C
+
+const app = compose(getData, mkUrl)
+
+// ALL ABOVE THIS LINE SHALL REMAIN PURE ==================
+
+;(function run() {
+  // TODO make city dynamic
+  app('London, uk').fork(console.error, console.log)
+})()
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js',
